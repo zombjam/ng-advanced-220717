cbadb52189b36ce2272dc8b2873fd130b0edd655
jamie <zomb16jam9@gmail.com>
2022-07-24 11:03:52 +0800

    adjust form input valid and invalid template to improve UX

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -9,8 +10,8 @@ export class LoginComponent implements OnInit, OnDestroy {
   orig_body_className = document.body.className;
 
   data: any = {
-    email: 'test@example.com',
-    password: '123',
+    email: '',
+    password: '',
     isRememberMe: true,
   };
 
@@ -28,5 +29,13 @@ export class LoginComponent implements OnInit, OnDestroy {
     localStorage.setItem('apikey', 'TEST');
     let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     this.router.navigateByUrl(returnUrl);
+  }
+
+  isInValid(ctrl: NgModel) {
+    return ctrl.invalid && ctrl.touched;
+  }
+
+  isValid(ctrl: NgModel) {
+    return ctrl.valid;
   }
 }
